8a9a4961426f9d8c13067f16278b41c2d9b7ff36
MarvinElRhumAdorator <marvingilly@yahoo.fr>
2021-05-03 23:15:24 +0200

    SOLID : init project

---
+++ src/main/java/fr/kaibee/solid/Runner.java
@@ -0,0 +1,4 @@
+package fr.kaibee.solid;
+
+public class Runner {
+}
---
+++ src/test/java/fr/kaibee/solid/RunnerTest.java
@@ -0,0 +1,12 @@
+package fr.kaibee.solid;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RunnerTest {
+    @Test
+    public void uselessTest() {
+        assertThat(true).isTrue();
+    }
+}
