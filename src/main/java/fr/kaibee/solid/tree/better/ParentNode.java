cce943a5c4f601843a6b45734f434504372e82e8
MarvinElRhumAdorator <marvingilly@yahoo.fr>
2021-05-05 11:02:08 +0200

    TREE : phase 4 - OPEN iterate process

--- src/main/java/fr/kaibee/solid/tree/better/ParentNode.java
+++ src/main/java/fr/kaibee/solid/tree/better/ParentNode.java
@@ -4,14 +4,13 @@ import fr.kaibee.solid.tree.Command;
 import fr.kaibee.solid.tree.Node;
 import fr.kaibee.solid.tree.Person;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class ParentNode implements Node {
 
-    private List<Node> nodes;
+    private final Collection<Node> nodes;
 
-    ParentNode(List<Node> nodes) {
+    ParentNode(Collection<Node> nodes) {
         if (nodes == null || nodes.isEmpty()) {
             throw new IllegalArgumentException("ParentNode should have children");
         }
@@ -20,13 +19,24 @@ public class ParentNode implements Node {
 
     @Override
     public Optional<Command> evaluate(Person person) {
-        return nodes
-                .stream()
-                .map(node -> node.evaluate(person))
-                .filter(Optional::isPresent)
-                .reduce((s, s2) -> {
-                    throw new IllegalStateException("Tree issue : several commands have been created");
-                })
-                .orElseGet(Optional::empty);
+        List<Command> commands = new ArrayList<>();
+        for (Iterator<Node> iterator = getNodeIterator(nodes); iterator.hasNext(); ) {
+            Node node = iterator.next();
+            node.evaluate(person).ifPresent(commands::add);
+        }
+
+        if (commands.isEmpty()) {
+            return Optional.empty();
+        }
+
+        if (commands.size() > 1) {
+            throw new IllegalStateException("Tree issue : several commands have been created");
+        }
+
+        return Optional.of(commands.get(0));
+    }
+
+    protected Iterator<Node> getNodeIterator(Collection<Node> nodes) {
+        return nodes.iterator();
     }
 }
